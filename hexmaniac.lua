ccd61eeef898790b88c6051914e628fcbe4875bb
JulianWebb <git@julianwebb.ca>
2019-03-19 01:01:43 -0700

    Overengineered the library Made it DRYer, and added version checking.

--- hexmaniac.lua
+++ hexmaniac.lua
@@ -1,30 +1,38 @@
 --Hex Maniac by LavenderTheGreat, feel free to remove this for compression purposes.
 local func = {}
 
-function func.rgb(stringtolove)
-	return {
-        (1/255)*tonumber(string.sub(stringtolove, 1, 2),16),
-        (1/255)*tonumber(string.sub(stringtolove, 3, 4),16),
-        (1/255)*tonumber(string.sub(stringtolove, 5, 6),16)
-    }
+local split = function (str, start, stop) 
+    return tonumber(string.sub(str, start, stop), 16)
 end
 
-function func.rgba(stringtolove)
-    return {
-        (1/255)*tonumber(string.sub(stringtolove, 1, 2),16),
-        (1/255)*tonumber(string.sub(stringtolove, 3, 4),16),
-        (1/255)*tonumber(string.sub(stringtolove, 5, 6),16),
-        (1/255)*tonumber(string.sub(stringtolove, 7, 8),16)
-    }
+local convert = function (num)
+    if love._version_major < 11 then
+        return num
+    else
+        return (1/255)*num
+    end
 end
 
-function func.rgbo(stringtolove, opacity)
-	return {
-        (1/255)*tonumber(string.sub(stringtolove, 1, 2),16),
-        (1/255)*tonumber(string.sub(stringtolove, 3, 4),16),
-        (1/255)*tonumber(string.sub(stringtolove, 5, 6),16),
-        opacity
-    }
+local apply = function (str, start, stop)
+    return convert(split(str, start, stop))
+end
+
+func.rgb = function (stringToLove)
+    local color = {}
+    for i=1, 5, 2 do
+        table.insert(color, apply(stringToLove, i, i+1))
+    end
+    return color
+end
+
+func.rgbo = function (stringToLove, opacity)
+    local color = func.rgb(stringToLove)
+    table.insert(color, opacity)
+    return color
+end
+
+func.rgba = function (stringToLove)
+    return func.rgbo(string.sub(stringToLove, 1, 6), apply(stringToLove, 7, 8))
 end
 
 return func
